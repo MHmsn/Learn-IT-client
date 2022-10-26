bfcec27834306608c1164de9c7122ad4bc814e56
MHmsn <moinulh.msn@gmail.com>
2022-10-27 00:10:11 +0600

    added react to pdf

--- src/components/CourseDetail/CourseDetail.js
+++ src/components/CourseDetail/CourseDetail.js
@@ -1,21 +1,35 @@
+import { createRef } from "react";
 import { React } from "react";
 import { FaCrown } from "react-icons/fa";
 import { Link, useLoaderData } from "react-router-dom";
-
+import ReactToPdf from "react-to-pdf";
 
 const CourseDetail = () => {
   const course = useLoaderData();
   const { name, img, details, videos, id } = course;
+  const ref = createRef();
   return (
-    <div className="bg-secondary p-10 rounded-lg">
+    <div className="bg-secondary p-10 rounded-lg" ref={ref}>
       <h1 className="text-5xl"> {name}</h1>
-      
+      <div className=" text-end">
+        <ReactToPdf targetRef={ref} filename={`${name}.pdf`}>
+          {({ toPdf }) => (
+            <button className="btn btn-primary" onClick={toPdf}>
+              Download
+            </button>
+          )}
+        </ReactToPdf>
+      </div>
       <br />
       <img src={img} className="w-1/2 mx-auto rounded-lg" alt="" />
       <p className="mt-8 text-2xl">Total Videos: {videos}</p>
       <p className="mt-8 text-2xl">{details}</p>
-      <div className="mt-8"> 
-        <Link to={`/courses/course-${id}/checkout`}><button className="btn btn-primary">Get Premium Access <FaCrown /></button></Link>
+      <div className="mt-8">
+        <Link to={`/courses/course-${id}/checkout`}>
+          <button className="btn btn-primary">
+            Get Premium Access <FaCrown />
+          </button>
+        </Link>
       </div>
     </div>
   );
