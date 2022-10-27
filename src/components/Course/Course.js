18e9db175eb32e1aae722c5d5605342d667c9980
MHmsn <moinulh.msn@gmail.com>
2022-10-27 21:34:40 +0600

    added a reasonable footer

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -6,7 +6,7 @@ import CourseCard from '../CourseCard/CourseCard';
 const Course = () => {
     const courses = useLoaderData();
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+        <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
             {
                 courses.map(course => <CourseCard key={course.id} course={course}></CourseCard>)
             }
