46afa90a93ac6439f9f47948776dbeb06e1e34a2
MHmsn <moinulh.msn@gmail.com>
2022-10-27 22:09:29 +0600

    final final edit

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+//blog page
+
 const Blog = () => {
     return (
         <div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import banner from '../../Assets/banner.jpg';
 
 //home page
 
@@ -8,7 +9,7 @@ const Home = () => {
     <div>
         <h2 className="font-bold text-3xl text-primary"> Welcome to Learn IT! Home for Science and Computer Technology Courses!</h2>
         <Link to="/courses"><button className="btn btn-secondary my-5"> Get Started with Courses</button></Link>
-        <img src="https://thumbs.dreamstime.com/b/science-technology-engineering-math-concept-outline-blue-banner-dark-background-vector-illustration-stem-151274081.jpg" className="my-10 rounded-lg mx-auto shadow-lg" alt="banner"/>
+        <img src={banner} className="my-10 rounded-lg mx-auto shadow-lg" alt="banner"/>
     </div>
   );
 };
